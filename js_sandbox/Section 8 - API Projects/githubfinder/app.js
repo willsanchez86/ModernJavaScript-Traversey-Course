6b16fd284fe37ad47487db30a469ca44a335ce11
Will <williamlancelotsanchez@gmail.com>
2022-05-11 01:45:31 -0400

    Version Complete

--- js_sandbox/Section 8 - API Projects/githubfinder/app.js
+++ js_sandbox/Section 8 - API Projects/githubfinder/app.js
@@ -1,3 +1,8 @@
+// Init Github
+const github = new Github;
+//Init UI
+const ui = new UI;
+
 // Search Input
 const searchUser = document.getElementById('searchUser');
 
@@ -7,6 +12,21 @@ searchUser.addEventListener('keyup', (e) => {
     const userText = e.target.value;
 
     if(userText !== '') {
-        console.log(userText);
+        // Make http call
+        github.getUser(userText)
+            .then(data => {
+                if(data.profile.message === 'Not Found') {
+                    // Show alert
+                    ui.showAlert('User not found', 'alert alert-danger');
+                } else {
+                    // Show Profile
+                    ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
+                }
+            })
+    } else {
+        // Clear Profile
+        ui.clearProfile();
     }
+
 })
--- js_sandbox/Section 8 - API Projects/githubfinder/github.js
+++ js_sandbox/Section 8 - API Projects/githubfinder/github.js
@@ -2,13 +2,21 @@ class Github {
     constructor() {
         this.client_id = 'dd7e28fd98b0195fb323';
         this.client_secret = '8607194b6d9a31fc31f4db13b70c94d61e79d532';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-        const profile = await profileResponse.json();
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-        return { profile }
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return { 
+            profile,
+            repos
+        }
     }
 }
